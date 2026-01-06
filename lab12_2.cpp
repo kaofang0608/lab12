0a906e043e1d39db730381ce7ed009748524f9c5
Mookanda <kaofang0608@gmail.com>
2026-01-06 13:17:23 +0700

    lab12_2.cppcompleted

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -1,9 +1,29 @@
 #include<iostream>
 using namespace std;
 
+int gcd(int,int);
+
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+int gcd(int x,int y){
+    if(y>x){
+        int z=0;
+        z=x;
+        x=y;
+        y=z;
+    }
+    while(x%y != 0){
+    int z=x%y;
+    x=y;
+    y=z;
+    
+    }
+    return y;
+    
+        
 }
